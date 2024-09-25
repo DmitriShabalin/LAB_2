b96aee03ff2f1ee0df7ac201dcc072a2f1859cb3
DmitriShabalin <dmitrijsabalin43689@gmail.com>
2024-09-25 17:26:11 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main() {
     double E = 6, Eo = 0.0885;
     double l = 13, C;
+    int i = 0;
     double r = 4.5;
     double R[9] = { 4.7, 4.8, 4.9, 5, 6, 7, 8, 9, 10 };
-    for (size_t i = 0; i < size(R); i++)
+    while(i < 9)
     {
         C = 2 * M_PI * E * Eo * l / log((R[i]) / r);
         cout << "R = " << R[i] << "   " << "C = " << C << endl;
+        ++i;
     }
 }
